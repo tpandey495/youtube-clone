10af56818fd9084969eba000a31e7f72f67414cb
Tarun Pandey <tpandey495@gmail.com>
2023-04-09 21:23:01 +0530

    tailwind configure

--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//  Head 
+//  Body
+//   Sidebar
+//     MenuItems
+//   MainContainer
+//     ButtonList
+//     VideoContainer
+//     VideoCard
