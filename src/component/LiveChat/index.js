a9871343834a7128c7c6a96a48b2dbb2a2047d6e
Tarun Pandey <tpandey495@gmail.com>
2023-04-25 11:41:40 +0530

    search suggestion fixed

---
+++ src/component/LiveChat/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LiveChat() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LiveChat;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,4 +4,4 @@ export const API_KEY=process.env.REACT_APP_API_KEY;
 export const YT_URL="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+API_KEY;
 
 export const search_api =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="; 
